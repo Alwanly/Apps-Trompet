743f17fc3eb7bfa671fdf9149a701e2d28b67a15
Robi Dewi Asih Pramesti <robidewiasih@gmail.com>
2020-02-18 12:04:19 +0700

    Activity-Registrasi ref #11

---
+++ android/app/src/main/java/com/example/rpl/trompey/BaseApiService.java
@@ -0,0 +1,9 @@
+package com.example.rpl.trompey;
+
+import android.app.DownloadManager;
+
+class BaseApiService {
+    public DownloadManager registerRequest(String toString, String toString1, String toString2, String toString3, String toString4) {
+        return null;
+    }
+}
