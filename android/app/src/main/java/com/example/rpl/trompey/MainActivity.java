ddbd0af3e1763f51394c3ce36822202123cef41a
Alwan Alyafi Mulyawan <alwanalyafi7@gmail.com>
2020-02-25 10:48:52 +0700

    Login Activity

--- android/app/src/main/java/com/example/rpl/trompey/MainActivity.java
+++ android/app/src/main/java/com/example/rpl/trompey/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.rpl.trompey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth mFirebaseAuth;
+    FirebaseUser mFirebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+            return;
+        } else {
+            Toast.makeText(this,mFirebaseUser.getDisplayName(),Toast.LENGTH_SHORT).show();
+        }
     }
 }
