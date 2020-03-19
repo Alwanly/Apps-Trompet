c570f4a2bf8101d157f3ab150330f030a06688a1
madedarmawijaya <soulessegoo@Gmail.com>
2020-03-19 21:13:56 +0700

    Edit xml pembayaran ref#4

---
+++ android/app/src/main/java/com/example/rpl/trompey/PembayaranMakananActivity.java
@@ -0,0 +1,14 @@
+package com.example.rpl.trompey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PembayaranMakananActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembayaran_makanan);
+    }
+}
