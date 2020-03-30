8c114034b752b0a7c3e23ac99c63eed2710239b7
madedarmawijaya <soulessegoo@Gmail.com>
2020-03-31 04:07:04 +0800

    Penambahan List View pada UI Pembelian Makanan ref#4

--- android/app/src/main/java/com/example/rpl/trompey/MainActivity.java
+++ android/app/src/main/java/com/example/rpl/trompey/MainActivity.java
@@ -1,29 +1,66 @@
 package com.example.rpl.trompey;
 
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.app.Activity;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.widget.TableLayout;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     FirebaseAuth mFirebaseAuth;
     FirebaseUser mFirebaseUser;
+
+    String[] member_names;
+    TypedArray profile_pics;
+    String[] status;
+    String[] contact_type;
+
+    List<RowItem> rowItems;
+    ListView mylistview;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//buat listview
+        rowItems = new ArrayList<RowItem>();
+        member_names = getResources().getStringArray(R.array.member_names);
+        profile_pics = getResources().obtainTypedArray(R.array.profile_pics);
+        status = getResources().getStringArray(R.array.status);
+        contact_type = getResources().getStringArray(R.array.contact_type);
+
+        for (int i = 0; i < member_names.length; i++) {
+            RowItem item = new RowItem(member_names[i],
+                    profile_pics.getResourceId(i, -1), status[i],
+                    contact_type[i]);
+            rowItems.add(item);
+        }
+        mylistview = (ListView) findViewById(R.id.list);
+        CostumAdapter adapterlist = new CostumAdapter(this, rowItems);
+        mylistview.setAdapter(adapterlist);
+
+        mylistview.setOnItemClickListener(this);
+
+
+
+
+
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
@@ -56,5 +93,12 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String member_name = rowItems.get(position).getMember_name();
+        Toast.makeText(getApplicationContext(), "" + member_name,
+                Toast.LENGTH_SHORT).show();
     }
 }
