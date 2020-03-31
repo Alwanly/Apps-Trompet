83b4251e21b2fb0716e5d61a66a260d6d6d381be
madedarmawijaya <soulessegoo@Gmail.com>
2020-03-31 15:29:52 +0800

    Solve error list view tidak tampil ref#4

--- android/app/src/main/java/com/example/rpl/trompey/MainActivity.java
+++ android/app/src/main/java/com/example/rpl/trompey/MainActivity.java
@@ -19,48 +19,16 @@ import com.google.firebase.auth.FirebaseUser;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity  {
 
     FirebaseAuth mFirebaseAuth;
     FirebaseUser mFirebaseUser;
-
-    String[] member_names;
-    TypedArray profile_pics;
-    String[] status;
-    String[] contact_type;
-
-    List<RowItem> rowItems;
-    ListView mylistview;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//buat listview
-        rowItems = new ArrayList<RowItem>();
-        member_names = getResources().getStringArray(R.array.member_names);
-        profile_pics = getResources().obtainTypedArray(R.array.profile_pics);
-        status = getResources().getStringArray(R.array.status);
-        contact_type = getResources().getStringArray(R.array.contact_type);
-
-        for (int i = 0; i < member_names.length; i++) {
-            RowItem item = new RowItem(member_names[i],
-                    profile_pics.getResourceId(i, -1), status[i],
-                    contact_type[i]);
-            rowItems.add(item);
-        }
-        mylistview = (ListView) findViewById(R.id.list);
-        CostumAdapter adapterlist = new CostumAdapter(this, rowItems);
-        mylistview.setAdapter(adapterlist);
-
-        mylistview.setOnItemClickListener(this);
-
-
-
-
-
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
@@ -95,10 +63,5 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         });
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String member_name = rowItems.get(position).getMember_name();
-        Toast.makeText(getApplicationContext(), "" + member_name,
-                Toast.LENGTH_SHORT).show();
-    }
+
 }
